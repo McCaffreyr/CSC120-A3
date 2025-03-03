520d266755bb714c93f947e6ab03108cccc593d6
Maggie McCaffrey <rmccaffrey@smith.edu>
2025-03-03 09:34:40 -0500

    fixed organization

--- Chatbot.java
+++ Chatbot.java
@@ -3,7 +3,7 @@ import java.util.List;
 interface Chatbot {
 
     void chat();
-    void printTranscript(List transcript);
-    String RandomResponse();
+    void printTranscript();
+    String randomResponse();
     
 }
--- Conversation.java
+++ Conversation.java
@@ -5,25 +5,41 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Conversation implements Chatbot {
+private List<String> responses;
+private List<String> transcript;
+private List<String> keywords;
+private List<String> newwords;
+
 
   // 
   /**
    * Constructor 
    */
   Conversation() {
-  }
+    this.responses = Arrays.asList("Gotcha gotcha.", "I see what you mean.", "Hmmmmmmm.", 
+    "That makes sense!");
+    this.transcript = new ArrayList<>();
+    this.keywords = Arrays.asList("I", "me", "am", "are", "you", "my", "your", "I'm");
+    this.newwords = Arrays.asList("You", "you", "are", "am", "I", "your", "my", "You're");
+  }    
 
   /**
    * Prints out a canned response each time it's called
    */
-  public String RandomResponse() {
+  public String randomResponse() {
     Random rand = new Random();
-    List<String> responses = Arrays.asList("Gotcha gotcha.", "I see what you mean.", "Hmmmmmmm.", 
-    "That makes sense!");
-    String randomElement = responses.get(rand.nextInt(responses.size()));
+    String randomElement = this.responses.get(rand.nextInt(responses.size()));
     return randomElement;
   }
 
+  /**
+   * Prints transcript of conversation
+   */
+  public void printTranscript() {
+    for (Integer num = 0; num < transcript.size(); num++) {
+      System.out.println(transcript.get(num));
+    }
+  }
 
   /**
    * Starts and runs the conversation with the user
@@ -32,73 +48,59 @@ class Conversation implements Chatbot {
   public void chat() {
     System.out.println("Welcome to this chatbot!");
     System.out.println("How many rounds of conversation would you like?");
-  }
+    try (Scanner input = new Scanner(System.in)) {
+        //getting the number of rounds wanted and starting the conversation
+        int rounds = input.nextInt();
+        //discarding the new line
+        input.nextLine();
+        System.out.println("\nGreat! How are you feeling?");
+        transcript.add("Great! How are you feeling");
 
-  /**
-   * Prints transcript of conversation
-   */
-  public void printTranscript(List transcript) {
-    for (Integer num = 0; num < transcript.size(); num++) {
-      System.out.println(transcript.get(num));
-    }
-  }
+        //completing the rounds of conversation
+        for (int i = 0; i < rounds; i++) {
+          String useranswer = input.nextLine();
+          transcript.add(useranswer);
 
+          //false if there are no keywords in the user's input
+          Boolean hasKeyword = false;
 
-  public static void main(String[] args) {
-    List<String> transcript = new ArrayList<>();
+          //if there are keywords in the user's input, it replaces them with the mirrored version
+          String[] answerarray = useranswer.split("\\s+");
+          List<String> answerlist = Arrays.asList(answerarray);
+            for (Integer j = 0; j < answerlist.size(); j++) {
+              if (keywords.contains(answerlist.get(j))) {
+                useranswer = useranswer.replace(answerlist.get(j), newwords.get(keywords.indexOf(answerlist.get(j))));
+                hasKeyword = true;
+              }
+            }
 
-    Conversation newconvo = new Conversation();
-    newconvo.chat();
-    Scanner input = new Scanner(System.in);
+            //if the user input doesn't contain any keywords, give a canned response
+            if (hasKeyword){
+              System.out.println(useranswer + "?");
+              transcript.add(useranswer + "?");
+            } else {
+              String randomresponse = randomResponse();
+              System.out.println(randomresponse);
+              transcript.add(randomresponse);
+            }
 
-    //list of key words and what they will be replaced by
-    List<String> keywords = Arrays.asList("I", "me", "am", "you", "my", "your", "I'm");
-    List<String> newwords = Arrays.asList("You", "you", "are", "I", "your", "my", "You're");
-    
-    //getting the number of rounds wanted and starting the conversation
-    int rounds = input.nextInt();
-    System.out.println("\nGreat! How are you feeling?");
-    transcript.add("Great! How are you feeling");
+          }
+      input.close();
 
-
-    //completing the rounds of conversation
-    for (Integer i = 0; i < rounds; i++) {
-      //i don't feel like i should need a second scanner, but it wouldn't pause for the user input with just the first scanner present
-      Scanner input2 = new Scanner(System.in);
-      String answer = input2.nextLine();
-      transcript.add(answer);
-
-      //false if there are no keywords in the user's input
-      Boolean hasKeyword = false;
-
-      //if there are keywords in the user's input, it replaces them with the mirrored version
-      String[] answerarray = answer.split("\\s+");
-      List<String> answerlist = Arrays.asList(answerarray);
-      for (Integer j = 0; j < answerlist.size(); j++) {
-        if (keywords.contains(answerlist.get(j))) {
-          answer = answer.replace(answerlist.get(j), newwords.get(keywords.indexOf(answerlist.get(j))));
-          hasKeyword = true;
-        }
-      }
-
-      //if the user input doesn't contain any keywords, give a canned response
-      if (hasKeyword){
-        System.out.println(answer + "?");
-        transcript.add(answer + "?");
-      } else {
-        String randomresponse = newconvo.RandomResponse();
-        System.out.println(randomresponse);
-        transcript.add(randomresponse);
-
-      }
+      
     }
 
     //final comments to user + transcript
     System.out.println("Goodbye!");
     transcript.add("Goodbye!");
     System.out.println("\nHere's a transcript of our conversation:");
+  }
 
-    newconvo.printTranscript(transcript);
+
+  public static void main(String[] args) {
+    Conversation newconvo = new Conversation();
+    newconvo.chat();
+    newconvo.printTranscript();
 
   }
 }
